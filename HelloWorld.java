82bb3f1d709885a5e541eb760e1311041f0dbf94
Harri Savolainen <97889552+harheisa@users.noreply.github.com>
2022-01-19 13:26:19 +0200

    separating javaprojects into their own branch

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Harri on 7.6.2017.
+ */
+
+import java.io.*;
+
+public class HelloWorld{
+	public static void main(String [ ] args) {
+
+        // tulostetaan tuttu tervehdys
+        System.out.print("Hello World! ");
+    }
+}
